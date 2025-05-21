61a284b43f3fce0c69baa2c3e169f4b938a8230f
ArpitKaul <akaul7407@gmail.com>
2025-05-21 17:30:11 +0530

    created bank app

---
+++ sentry.client.config.ts
@@ -0,0 +1,54 @@
+// // This file configures the initialization of Sentry on the client.
+// // The config you add here will be used whenever a users loads a page in their browser.
+// // https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+// import * as Sentry from "@sentry/nextjs";
+
+// Sentry.init({
+//   dsn: "https://e300b41df2ffb6d4592972400a25c0d3@o4506876178464768.ingest.us.sentry.io/4507159179034624",
+
+//   // Adjust this value in production, or use tracesSampler for greater control
+//   tracesSampleRate: 1,
+
+//   // Setting this option to true will print useful information to the console while you're setting up Sentry.
+//   debug: false,
+
+//   replaysOnErrorSampleRate: 1.0,
+
+//   // This sets the sample rate to be 10%. You may want this to be 100% while
+//   // in development and sample at a lower rate in production
+//   replaysSessionSampleRate: 0.1,
+
+//   // You can remove this option if you're not planning to use the Sentry Session Replay feature:
+//   integrations: [
+//     Sentry.replayIntegration({
+//       // Additional Replay configuration goes in here, for example:
+//       maskAllText: true,
+//       blockAllMedia: true,
+//     }),
+//   ],
+// });
+
+
+// Sentry is disabled for development and debugging purposes.
+// To re-enable it, remove the conditional check or restore the original config.
+
+import * as Sentry from "@sentry/nextjs";
+
+if (process.env.NODE_ENV === "production") {
+  Sentry.init({
+    dsn: "https://e300b41df2ffb6d4592972400a25c0d3@o4506876178464768.ingest.us.sentry.io/4507159179034624",
+
+    tracesSampleRate: 1,
+    debug: false,
+    replaysOnErrorSampleRate: 1.0,
+    replaysSessionSampleRate: 0.1,
+
+    integrations: [
+      Sentry.replayIntegration({
+        maskAllText: true,
+        blockAllMedia: true,
+      }),
+    ],
+  });
+}
